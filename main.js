72aa67a37776897dd4a1ff6254fd608231892750
syn <vicdemilly@gmail.com>
2019-04-09 16:35:53 +0200

    Correction, autre methode plus nav friendly

--- main.js
+++ main.js
@@ -1,7 +1,16 @@
-function formulaire()
-{
-  let nom = lastname.value
-  let prenom = firstname.value
-  let ville = city.value
-  alert(`Nom : ${nom} \nPrénom : ${prenom} \nVille : ${ville}`)
+function Display(){
+  let nom = document.getElementById('lastname').value;
+  let prenom = document.getElementById('firstname').value;
+  let ville = document.querySelector('#city').value;
+  alert(nom + "\n" + prenom + "\n" + ville);
 }
+
+
+/*function Display()
+{
+  let nom = lastname.value;
+  let prenom = firstname.value;
+  let ville = city.value;
+  alert(`Nom : ${nom} \nPrénom : ${prenom} \nVille : ${ville}`);
+}*/
+// Ceci est de l'interpolation
